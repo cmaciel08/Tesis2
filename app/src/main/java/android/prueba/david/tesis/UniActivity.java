58ad5e39eee9e39afa6514f038550fad2b2a0910
Cristian Maciel <davidmacielmeza@gmail.com>
2017-09-17 19:31:44 -0400

    cambios en la vista de uni

--- app/src/main/java/android/prueba/david/tesis/UniActivity.java
+++ app/src/main/java/android/prueba/david/tesis/UniActivity.java
@@ -2,8 +2,18 @@ package android.prueba.david.tesis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UniActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +22,32 @@ public class UniActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_logo);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        //Datos a mostrar
+        List<String> names = new ArrayList<String>();
+        names.add("Cuota Administracion 60.000");
+        names.add("Cuota Ing. Comercial 60.000");
+        names.add("Cuota Contaduria 60.000");
+        names.add("Matricula Administracion 250.000");
+        names.add("Matriula Contaduria 250.000");
+        names.add("Matriula Ing. Comercial 300.000");
+        names.add("Derecho de Ex. Administracion P.O. 35.000");
+        names.add("Derecho de Ex. Contaduria P.O. 35.000");
+        names.add("Derecho de Ex. Ing. Comercial P.O. 35.000");
+        names.add("Derecho de Ex. Administracion S.O. 35.000");
+        names.add("Derecho de Ex. Contaduria S.O. 35.000");
+        names.add("Derecho de Ex. Ing. Comercial S.O. 35.000");
+        names.add("Derecho de Ex. Administracion T.O. 35.000");
+        names.add("Derecho de Ex. Contaduria T.O. 35.000");
+        names.add("Derecho de Ex. Ing. Comercial T.O. 35.000");
+
+
+        // Adaptador de la forma visul en que mostraremos nuestro datos
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+
+        // Enlasamos el adaptador con nuestro listView
+        listView.setAdapter(adapter);
     }
 }
