3c0c11288e4ed2e3157e06194c66ec2c05cc53e2
Cristian Maciel <davidmacielmeza@gmail.com>
2017-09-16 20:39:41 -0400

    agreue los activity para UNI, MUNI y PJ

---
+++ app/src/main/java/android/prueba/david/tesis/MuniActivity.java
@@ -0,0 +1,13 @@
+package android.prueba.david.tesis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MuniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muni);
+    }
+}
---
+++ app/src/main/java/android/prueba/david/tesis/PjudicialActivity.java
@@ -0,0 +1,13 @@
+package android.prueba.david.tesis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PjudicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pjudicial);
+    }
+}
