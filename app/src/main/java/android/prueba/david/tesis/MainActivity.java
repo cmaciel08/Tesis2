80b8a560309109800a4f45bd4dbc756b6205fad6
Cristian Maciel <davidmacielmeza@gmail.com>
2017-09-16 21:50:01 -0400

    hasype le quite el boton que molestaba de la vista principal

--- app/src/main/java/android/prueba/david/tesis/MainActivity.java
+++ app/src/main/java/android/prueba/david/tesis/MainActivity.java
@@ -28,14 +28,6 @@ public class MainActivity extends AppCompatActivity
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_logo);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
