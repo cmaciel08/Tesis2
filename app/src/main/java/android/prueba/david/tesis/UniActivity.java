7a11aaad8301b8cbf1f7142f5163a12017607636
Cristian Maciel <davidmacielmeza@gmail.com>
2017-10-04 19:16:54 -0300

    agregue buscar en la vista de uni

--- app/src/main/java/android/prueba/david/tesis/UniActivity.java
+++ app/src/main/java/android/prueba/david/tesis/UniActivity.java
@@ -3,6 +3,10 @@ package android.prueba.david.tesis;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
@@ -15,6 +19,7 @@ public class UniActivity extends AppCompatActivity {
 
     private ListView listView;
     private ImageButton imgButton;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,7 @@ public class UniActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_logo);
 
-        listView = (ListView) findViewById(R.id.listView);
+        ListView lv = (ListView) findViewById(R.id.listViewCosto);
 
         //Datos a mostrar
         List<String> names = new ArrayList<String>();
@@ -55,10 +60,40 @@ public class UniActivity extends AppCompatActivity {
 
 
         // Adaptador de la forma visul en que mostraremos nuestro datos
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        adapter = new ArrayAdapter<>(
+                UniActivity.this,
+                android.R.layout.simple_list_item_1,
+                names);
+        lv.setAdapter(adapter);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
 
         // Enlasamos el adaptador con nuestro listView
-        listView.setAdapter(adapter);
+        //listView.setAdapter(adapter);
 
     }
+
+   @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       MenuInflater inflater = getMenuInflater();
+       inflater.inflate(R.menu.menu_search, menu);
+       MenuItem item = menu.findItem(R.id.menuSearch);
+       SearchView searchView = (SearchView) item.getActionView();
+
+       searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+           @Override
+           public boolean onQueryTextSubmit(String query) {
+               return false;
+           }
+
+           @Override
+           public boolean onQueryTextChange(String newText) {
+               adapter.getFilter().filter(newText);
+               return false;
+           }
+       });
+
+
+       return super.onCreateOptionsMenu(menu);
+   }
 }
+
